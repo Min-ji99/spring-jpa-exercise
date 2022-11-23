f3a541042f77f235cc9435d7c609bba4e27dce9a
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-11-23 14:34:25 +0900

    Feat : 출판사 이름 추가

--- src/main/java/com/jpa/exercise/domain/dto/BookResponse.java
+++ src/main/java/com/jpa/exercise/domain/dto/BookResponse.java
@@ -14,11 +14,13 @@ public class BookResponse {
     private Integer bookId;
     private String bookName;
     private String authorName;
+    private String publisherName;
     public static BookResponse of(Book book){
         return BookResponse.builder()
                 .bookId(book.getId())
                 .bookName(book.getBookName())
                 .authorName(book.getAuthor().getAuthorName())
+                .publisherName(book.getPublisher().getPublisherName())
                 .build();
     }
 }
