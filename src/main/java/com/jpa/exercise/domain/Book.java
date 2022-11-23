2b38131685c8d65f19064a43007c2ccdbfe8e805
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-11-23 15:03:50 +0900

    Update : OneToOne 매핑으로 수정

--- src/main/java/com/jpa/exercise/domain/Book.java
+++ src/main/java/com/jpa/exercise/domain/Book.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 @Entity
 @Getter
@@ -20,7 +17,7 @@ public class Book {
     @ManyToOne
     @JoinColumn(name="author_id")
     private Author author;
-    @ManyToOne
+    @OneToOne
     @JoinColumn(name="publisher_id")
     private Publisher publisher;
 }
