3d1ee206c3849307202f3ad85b8fd9011577810b
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-11-24 15:51:19 +0900

    Feat : Swagger 적용

--- src/main/java/com/jpa/exercise/SpringJpaExerciseApplication.java
+++ src/main/java/com/jpa/exercise/SpringJpaExerciseApplication.java
@@ -1,5 +1,6 @@
 package com.jpa.exercise;
 
+import com.jpa.exercise.config.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
